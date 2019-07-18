e5a4985adc511dc94d0b16c10b1043764d9d7a7e
devopsmd <52909878+devopsmd@users.noreply.github.com>
2019-07-19 01:53:03 +0530

    commit23

---
+++ server/src/main/java/com/example/Guru_register.java
@@ -0,0 +1,42 @@
+package com.example;
+
+ 
+import java.io.IOException;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * Servlet implementation class guru_register
+ */
+public class guru_register extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String first_name = request.getParameter("first_name");
+		String last_name = request.getParameter("last_name");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String address = request.getParameter("address");
+		String contact = request.getParameter("contact");
+		
+		if(first_name.isEmpty() || last_name.isEmpty() || username.isEmpty() || 
+				password.isEmpty() || address.isEmpty() || contact.isEmpty())
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_1.jsp");
+			req.include(request, response);
+		}
+		else
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_2.jsp");
+			req.forward(request, response);
+		}
+	}
+ 
+}
+© 2019 GitHub, Inc.
