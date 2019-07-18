cbcd8ba47350f3080be391cd9ced43d25631e2b7
devopsmd <52909878+devopsmd@users.noreply.github.com>
2019-07-19 01:51:41 +0530

    commit 4

---
+++ server/src/main/java/com/example/Guru_login.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import java.io.IOException;
+ 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+/**
+ * Servlet implementation class guru_login
+ */
+public class guru_login extends HttpServlet {
+ 
+    public guru_login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+ 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if(username.isEmpty() || password.isEmpty() )
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_3.jsp");
+			req.include(request, response);
+		}
+		else
+		{
+			RequestDispatcher req = request.getRequestDispatcher("register_4.jsp");
+			req.forward(request, response);
+		}
+	}
+ 
+}
